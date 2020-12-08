2d59e247d05226855658bf4a0cf36743dbfd0bfe
Sugiyama, Henrique <henrique.sugiyama@dieboldnixdorf.com>
2020-12-08 11:59:42 -0300

    second

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -10,28 +10,28 @@ const Home: React.FC = () => {
       <SubTitle>Supreme</SubTitle>
       <SubTitle>Stuffs</SubTitle>
       <Section>
-        <img src="https://64.media.tumblr.com/acac4699bef78c03736662b9a4d7c56f/tumblr_pe2bcnb7oi1s30ko5o1_500.gif" width="100%" height="auto" />
-        <img src="https://64.media.tumblr.com/d16126c7de94aa6553bf94d1cdce5fe8/tumblr_pe93buCLay1uocsx1o1_500.gif" width="50%" height="auto" />
-        <img src="https://64.media.tumblr.com/4946d7e76bdcdd5a2aa1f9f2874d1c9b/tumblr_pe2c7qer8C1s30ko5o1_500.gif" width="50%" height="auto"/>
-        <img src="https://64.media.tumblr.com/86c763bf3caa0a20a3c8901fd6b8f8d1/tumblr_oncecnJZJ91s30ko5o1_400.gif" width="50%" height="auto"/>
-        <img src="https://64.media.tumblr.com/0ff642e1e60c525615e136d3bc7b8c86/tumblr_pmo49hmMDK1rchkzlo1_400.gif" width="50%" height="auto"/>
-        <img src="https://64.media.tumblr.com/d99cdc3daaa07fd043d43ae760f954c9/tumblr_pni14dgBvA1sjwwzso1_500.gif" width="100%" height="auto"/>
-        <img src="https://64.media.tumblr.com/900f8c5c8e931ffe60eb95f29f815a08/tumblr_opduayjjdA1vm1ubqo1_500.gif" width="30%" height="auto" style={{marginLeft: 25, marginBottom: 25}}/>
-        <img src="https://64.media.tumblr.com/a31c3cdeab3ccb2b390b43ca1a7404e5/tumblr_osa61l9Eyb1qcjlv3o1_400.gif" width="40%" height="auto" style={{marginLeft: 25}} />
-        <img src="https://64.media.tumblr.com/7f8b6557cb99eb2216ae2d33c51eed4d/tumblr_p2t6ztcDmD1uwi3dvo1_500.gifv" width="40%" height="auto" />
-        <img src="https://64.media.tumblr.com/9687d0da38132d24fbd0513aed9ee0f9/tumblr_otzf5ceU4t1qztvpwo1_500.gif" width="50%" height="auto"/>
-        <img src="https://64.media.tumblr.com/26d69c9efb41ef14eece862fea4cb5d8/tumblr_oljfxikDTq1saq3w2o1_500.gif" width="25%" height="auto"/>
-        <img src="http://intern-mag.com/wp-content/uploads/2018/11/3_1.gif" width="75%" height="auto" />
+        <img src="https://64.media.tumblr.com/acac4699bef78c03736662b9a4d7c56f/tumblr_pe2bcnb7oi1s30ko5o1_500.gif" width="100%" height="auto" alt="Supreme Stuffs" />
+        <img src="https://64.media.tumblr.com/d16126c7de94aa6553bf94d1cdce5fe8/tumblr_pe93buCLay1uocsx1o1_500.gif" width="50%" height="auto" alt="Supreme Stuffs"/>
+        <img src="https://64.media.tumblr.com/4946d7e76bdcdd5a2aa1f9f2874d1c9b/tumblr_pe2c7qer8C1s30ko5o1_500.gif" width="50%" height="auto"alt="Supreme Stuffs"/>
+        <img src="https://64.media.tumblr.com/86c763bf3caa0a20a3c8901fd6b8f8d1/tumblr_oncecnJZJ91s30ko5o1_400.gif" width="50%" height="auto" alt="Supreme Stuffs"/>
+        <img src="https://64.media.tumblr.com/0ff642e1e60c525615e136d3bc7b8c86/tumblr_pmo49hmMDK1rchkzlo1_400.gif" width="50%" height="auto" alt="Supreme Stuffs"/>
+        <img src="https://64.media.tumblr.com/d99cdc3daaa07fd043d43ae760f954c9/tumblr_pni14dgBvA1sjwwzso1_500.gif" width="100%" height="auto" alt="Supreme Stuffs"/>
+        <img src="https://64.media.tumblr.com/900f8c5c8e931ffe60eb95f29f815a08/tumblr_opduayjjdA1vm1ubqo1_500.gif" width="30%" height="auto" alt="Supreme Stuffs" style={{marginLeft: 25, marginBottom: 25}}/>
+        <img src="https://64.media.tumblr.com/a31c3cdeab3ccb2b390b43ca1a7404e5/tumblr_osa61l9Eyb1qcjlv3o1_400.gif" width="40%" height="auto" alt="Supreme Stuffs" style={{marginLeft: 25}} />
+        <img src="https://64.media.tumblr.com/7f8b6557cb99eb2216ae2d33c51eed4d/tumblr_p2t6ztcDmD1uwi3dvo1_500.gifv" width="40%" height="auto" alt="Supreme Stuffs" />
+        <img src="https://64.media.tumblr.com/9687d0da38132d24fbd0513aed9ee0f9/tumblr_otzf5ceU4t1qztvpwo1_500.gif" width="50%" height="auto" alt="Supreme Stuffs"/>
+        <img src="https://64.media.tumblr.com/26d69c9efb41ef14eece862fea4cb5d8/tumblr_oljfxikDTq1saq3w2o1_500.gif" width="25%" height="auto" alt="Supreme Stuffs"/>
+        <img src="http://intern-mag.com/wp-content/uploads/2018/11/3_1.gif" width="75%" height="auto" alt="Supreme Stuffs"/>
         <SubTitle>Henrique Sugiyama</SubTitle>
         <SubTitle>Henrique Sugiyama</SubTitle>
         <Title>Supreme</Title>
         <Title>Stuffs</Title>
-        <img src="https://joecoffeecompany.com/wp-content/uploads/2018/08/joe-coffee-V60-drawdown-serve-step-7.gif" width="25%" height="auto"/>
-        <img src="https://joecoffeecompany.com/wp-content/uploads/2018/08/joe-coffee-V60-drawdown-serve-step-7.gif" width="25%" height="auto"/>
-        <img src="https://joecoffeecompany.com/wp-content/uploads/2018/08/joe-coffee-V60-drawdown-serve-step-7.gif" width="25%" height="auto"/>
-        <img src="https://joecoffeecompany.com/wp-content/uploads/2018/08/joe-coffee-V60-drawdown-serve-step-7.gif" width="25%" height="auto"/>
-        <img src=" http://d3pv3ojoz7l5mn.cloudfront.net/wp-content/uploads/2015/09/17213633/BelugaStopMotion.gif" width="25%" height="auto"/>
-        <img src="https://1zbuhu1t3cn216t5533fsizp-wpengine.netdna-ssl.com/wp-content/uploads/2017/10/BL_BrewGuide_Chemex_9_animate740.gif" width="75%" height="auto"/>
+        <img src="https://joecoffeecompany.com/wp-content/uploads/2018/08/joe-coffee-V60-drawdown-serve-step-7.gif" width="25%" height="auto" alt="Supreme Stuffs"/>
+        <img src="https://joecoffeecompany.com/wp-content/uploads/2018/08/joe-coffee-V60-drawdown-serve-step-7.gif" width="25%" height="auto" alt="Supreme Stuffs"/>
+        <img src="https://joecoffeecompany.com/wp-content/uploads/2018/08/joe-coffee-V60-drawdown-serve-step-7.gif" width="25%" height="auto" alt="Supreme Stuffs"/>
+        <img src="https://joecoffeecompany.com/wp-content/uploads/2018/08/joe-coffee-V60-drawdown-serve-step-7.gif" width="25%" height="auto" alt="Supreme Stuffs"/>
+        <img src=" http://d3pv3ojoz7l5mn.cloudfront.net/wp-content/uploads/2015/09/17213633/BelugaStopMotion.gif" width="25%" height="auto" alt="Supreme Stuffs"/>
+        <img src="https://1zbuhu1t3cn216t5533fsizp-wpengine.netdna-ssl.com/wp-content/uploads/2017/10/BL_BrewGuide_Chemex_9_animate740.gif" width="75%" height="auto" alt="Supreme Stuffs"/>
         <SubTitle>Bye</SubTitle>
       </Section>
     </>
